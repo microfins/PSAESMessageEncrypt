8979e73472ee9044cc3a6467dc3a87ba5065d0c5
shwnstrmn <shawn.esterman@gmail.com>
2016-02-15 22:17:04 -0600

    Fixed issue with module import

--- PSAESMessageEncrypt/PSAESMessageEncrypt.psm1
+++ PSAESMessageEncrypt/PSAESMessageEncrypt.psm1
@@ -1,13 +1,4 @@
-#Get public and private function definition files.
-    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-
-#Dot source the files
-    Foreach($import in @($Public + $Private)) {
-        Try {
-            . $import.fullname
-        } Catch {
-            Write-Error -Message "Failed to import function $($import.fullname): $_"
-        }
-    }
-
-Export-ModuleMember -Function $Public.Basename
+Foreach ( $Script in @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue ) ) {
+    Try { . $Script.FullName }
+    Catch { Write-Error -Message "Failed to import function $($Script.FullName): $_" }
+}
